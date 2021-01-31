450abb35c8d4a9160fb49248824f18bdb7379dff
Kalabuk Dmitriy <kalabukdima@gmail.com>
2021-01-31 19:19:12 +0300

    Implement unique_ptr

--- dsalglib/include/alginc.h
+++ dsalglib/include/alginc.h
@@ -12,13 +12,34 @@
 namespace dsa
 {
 
+    template <class T>
+    struct remove_reference {
+        using type = T;
+    };
+    template <class T>
+    struct remove_reference<T&> {
+        using type = T;
+    };
+    template <class T>
+    struct remove_reference<T&&> {
+        using type = T;
+    };
+
+    // Analogue of std::move
+    template <class T>
+    typename remove_reference<T>::type&&
+    rvalue_cast(T&& t) noexcept
+    {
+        return static_cast<typename remove_reference<T>::type&&>(t);
+    }
+
 
     template<typename T>
     inline void swapit(T &x, T &y)
     {
-        T t = x;
-        x = y;
-        y = t;
+        T t = rvalue_cast(x);
+        x = rvalue_cast(y);
+        y = rvalue_cast(t);
     }
 
     template<typename T>
@@ -35,6 +56,18 @@ namespace dsa
         T min;
         min = (x < y) ? x : y;
         return min;
+    }
+
+    // Analogue of std::forward.
+    template<class T>
+    T&& forward(typename remove_reference<T>::type& t) noexcept
+    {
+        return static_cast<T&&>(t);
+    }
+    template<class T>
+    T&& forward(typename remove_reference<T>::type&& t) noexcept
+    {
+        return static_cast<T&&>(t);
     }
 
 }
